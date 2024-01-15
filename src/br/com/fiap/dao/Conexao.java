7f1da27dba856a29bc0a37b1b76d21360fb30439
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-01-15 10:21:11 -0300

    Conexão aberta criada

---
+++ src/br/com/fiap/dao/Conexao.java
@@ -0,0 +1,29 @@
+package br.com.fiap.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	public static Connection abrirConexao() {
+		Connection con = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+			final String USER = "email";
+			final String PASS = "senha";
+			con = DriverManager.getConnection(url, USER, PASS);
+			System.out.println("Conexão aberta");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return con;
+	}
+		
+}
