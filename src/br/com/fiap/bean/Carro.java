c6045b12081510c3071bd4a7e1c4a64656fb2b0a
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-01-15 10:39:14 -0300

    Getters e setters criados

---
+++ src/br/com/fiap/bean/Carro.java
@@ -0,0 +1,34 @@
+package br.com.fiap.bean;
+
+public class Carro {
+	private String placa;
+	private String cor;
+	private String descricao;
+	
+	//Getters e setters
+	public String getPlaca() {
+		return placa;
+	}
+	
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
