c7775027cac771fbc922137c20f8b4ab093172d5
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-01-16 08:56:28 -0300

    Classe de teste para o método listarTodos

---
+++ src/br/com/fiap/main/teste4.java
@@ -0,0 +1,29 @@
+package br.com.fiap.main;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import br.com.fiap.bean.Carro;
+import br.com.fiap.dao.CarroDAO;
+import br.com.fiap.dao.Conexao;
+
+public class teste4 {
+	public static void main(String[] args) {
+		Connection con = Conexao.abrirConexao();
+		
+		CarroDAO cd = new CarroDAO(con);
+		
+		//Teste do método ListarTodos
+		ArrayList<Carro> lista = cd.listarTodos();
+		
+		if(lista != null) {
+			for (Carro carro : lista) {
+				System.out.println("Placa: " + carro.getPlaca());
+				System.out.println("Cor: " + carro.getCor());
+				System.out.println("Descrição: " + carro.getDescricao());
+			}
+		}
+		
+		Conexao.fecharConexao(con);
+	}
+}
